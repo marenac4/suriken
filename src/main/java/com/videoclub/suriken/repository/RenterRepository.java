8cf9c9e59fc54d8d32cdab1933c836f860287de4
marenac4 <lazovicmare4@gmail.com>
2021-05-27 12:50:06 +0200

    Added new method for query renter by its email

--- src/main/java/com/videoclub/suriken/repository/RenterRepository.java
+++ src/main/java/com/videoclub/suriken/repository/RenterRepository.java
@@ -10,6 +10,10 @@ import java.util.Optional;
 @Repository
 public interface RenterRepository extends JpaRepository<MovieRenter, Long> {
 
+    Optional<MovieRenter> findMovieRenterByFirstName(String firstName);
+
+    Optional<MovieRenter> findMovieRenterByMail(String mail);
+
     @Query("SELECT r FROM MovieRenter r LEFT JOIN FETCH r.rentedMovies m WHERE r.id = ?1")
     Optional<MovieRenter> findRenterByIdAndRentedMovies(Long renterId);
 }
